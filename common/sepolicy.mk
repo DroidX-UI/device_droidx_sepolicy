2bcd2818dae269e1e9d6975567c9e82bea999838
Michael Bestas <mkbestas@gmail.com>
2021-11-10 23:32:44 +0200

    Remove legacy sepolicy leftover
    Change-Id: I2913565cc402b29acd380015b049ccf8d4e9959d
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -25,9 +25,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/droidx/sepolicy/common/vendor
 endif
 
-# Selectively include legacy rules defined by the products
--include device/droidx/sepolicy/legacy-common/sepolicy.mk
-
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
 include device/droidx/sepolicy/atv/sepolicy.mk
