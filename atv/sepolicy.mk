cd21432733475d70af406f1a8fa512505e405f17
acex88 <abhinav.115260@gmail.com>
2024-01-22 09:21:13 +0530

    sepolicy: Adapt for DroidX-UI
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,12 +1,12 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from Droidx
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/droidx/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/droidx/sepolicy/atv/private
